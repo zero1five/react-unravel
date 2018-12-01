0b6d45ee06b38788e34527a76b3b8c75a450aecf
zero1five <zerodengyin@gmail.com>
2018-12-01 20:18:34 +0800

    git: update shell

--- shell.js
+++ shell.js
@@ -1,58 +1,58 @@
 const {
-	execSync
+  execSync
 } = require('child_process')
 const readLine = require('readline')
 
 const compose = (...funcs) =>
-	funcs.reduce((a, b) => (...args) => a(b(...args)), arg => arg)
+  funcs.reduce((a, b) => (...args) => a(b(...args)), arg => arg)
 
 const readSyncByRl = (tips = '>>> ') => new Promise(res => {
-	const rl = readLine.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	})
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
 
-	rl.question(tips, answer => {
-		rl.close()
-		res(answer.trim())
-	})
+  rl.question(tips, answer => {
+    rl.close()
+    res(answer.trim())
+  })
 })
 
 const visitSets = cmd => {
-	const visitors = {
-		tree: "tree -I 'README.md|shell.js|scripts'",
-		push: "node ./scripts/push"
-	}
+  const visitors = {
+    tree: "tree -I 'README.md|shell.js|scripts'",
+    push: "node ./scripts/push"
+  }
 
-	return visitors[cmd]
+  return visitors[cmd]
 }
 
 const inquriy = async () => {
-	const cmd = await readSyncByRl('Please enter a command (e.g: tree | push) ： \n')
+  const cmd = await readSyncByRl('Please enter a command (e.g: tree | push) ： \n')
 
-	return {
-		cmd
-	}
+  return {
+    cmd
+  }
 }
 
 /**
  * run command
  */
 const runCmd = async cmd => execSync(cmd, {
-	stdio: 'inherit'
+  stdio: 'inherit'
 })
 
 
 const App = async () => {
-	const {
-		cmd
-	} = await inquriy()
+  const {
+    cmd
+  } = await inquriy()
 
-	const result = await compose(
-		runCmd, visitSets
-	)(cmd)
+  const result = await compose(
+    runCmd, visitSets
+  )(cmd)
 
-	return result
+  return result
 }
 
 
